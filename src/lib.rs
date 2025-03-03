cda572de7ca68c3322d7212281cf3f85ee03b5f4
Michael Lodder <mike@litprotocol.com>
2025-03-03 12:27:41 -0700

    conditionally export inner curves
    Signed-off-by: Michael Lodder <mike@litprotocol.com>

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,31 @@ pub use derive::*;
 #[cfg(feature = "ecops")]
 pub use ec_ops::*;
 
+#[cfg(feature = "p256")]
+pub extern crate p256;
+
+#[cfg(feature = "p384")]
+pub extern crate p384;
+
+#[cfg(feature = "k256")]
+pub extern crate k256;
+
+#[cfg(feature = "curve25519")]
+pub use vsss_rs::curve25519;
+
+#[cfg(feature = "bls")]
+pub extern crate blsful;
+
+#[cfg(feature = "ed448")]
+pub extern crate ed448_goldilocks_plus;
+
+#[cfg(feature = "jubjub")]
+pub extern crate jubjub;
+
+#[cfg(feature = "decaf377")]
+pub extern crate decaf377;
+
+
 #[cfg(not(any(
     feature = "p256",
     feature = "p384",
